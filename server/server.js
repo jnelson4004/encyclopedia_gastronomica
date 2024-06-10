5595c874ad67f95eb100b62450f65189ab7baf72
burt reynolds <jnelson4004@gmail.com>
2024-06-09 20:16:25 -0500

    one more time

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { Recipe } = require('./models');
 const routes = require('./routes');
 const userRoutes = require('./controllers/user.Controller');
 const recipeRoutes = require('./controllers/recipe.Controller');
-const commentRoutes = require('./controllers/comments.Controller');
+const commentRoutes = require('./controllers/comments.controller');
 
 
 const PORT = process.env.PORT || 3001;
